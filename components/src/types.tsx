c226c814925aca0ec9a4cf3633fb53563dfe56ae
annatvali <anntvaliashvili@gmail.com>
2023-11-01 23:23:17 +0400

    feat: add planets and types

---
+++ components/src/types.tsx
@@ -0,0 +1,20 @@
+export interface PlanetBase {
+  climate: string;
+  created: string;
+  diameter: string;
+  edited: string;
+  films: string[];
+  gravity: string;
+  name: string;
+  orbital_period: string;
+  population: string;
+  residents: string[];
+  rotation_period: string;
+  surface_water: string;
+  terrain: string;
+  url: string;
+}
+
+export interface Planet extends PlanetBase {
+  id: string;
+}
