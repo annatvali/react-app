526a3ccca3b8e304ffc4001dfab644e2547c0708
annatvali <anntvaliashvili@gmail.com>
2023-11-03 11:47:16 +0400

    refactor: migrate class components to function components

--- components/src/Planets.tsx
+++ components/src/Planets.tsx
@@ -1,4 +1,3 @@
-import { Component, ReactElement } from 'react';
 import { Planet } from './types';
 import { getPlanetImage } from './api';
 
@@ -6,40 +5,36 @@ interface PlanetCardProps {
   planet: Planet;
 }
 
-class PlanetCard extends Component<PlanetCardProps> {
-  render(): ReactElement {
-    const { id, name, diameter, population } = this.props.planet;
-    return (
-      <div className="planetCard">
-        <div className="planetView">
-          <img className="planetImage" src={getPlanetImage(id)} />
-          <div className="planetName">{name}</div>
-        </div>
-        <div className="planetInfo">
-          <div>
-            Diameter: {diameter}
-            {diameter !== 'unknown' && 'km'}
-          </div>
-          <div>Population: {population}</div>
-        </div>
+export const PlanetCard: React.FC<PlanetCardProps> = ({ planet }) => {
+  const { id, name, diameter, population } = planet;
+  return (
+    <div className="planetCard">
+      <div className="planetView">
+        <img className="planetImage" src={getPlanetImage(id)} />
+        <div className="planetName">{name}</div>
       </div>
-    );
-  }
+      <div className="planetInfo">
+        <div>
+          Diameter: {diameter}
+          {diameter !== 'unknown' && 'km'}
+        </div>
+        <div>Population: {population}</div>
+      </div>
+    </div>
+  )
 }
 
 interface PlanetsProps {
   planets: Planet[];
 }
 
-export default class Planets extends Component<PlanetsProps> {
-  render(): ReactElement {
-    console.log('Planets');
-    return (
-      <div className="planets">
-        {this.props.planets.map((p) => (
-          <PlanetCard key={p.id} planet={p} />
-        ))}
-      </div>
-    );
-  }
+const Planets: React.FC<PlanetsProps> = ({ planets }) => {
+  return (
+    <div className='planets'>
+      {planets.map((p) =>
+        <PlanetCard key={p.id} planet={p} /> )}
+    </div>
+  )
 }
+
+export default Planets;
--- components/src/SearchForm.tsx
+++ components/src/SearchForm.tsx
@@ -1,58 +1,45 @@
-import { ChangeEvent, Component, FormEvent, ReactElement } from 'react';
+import { useState, useEffect } from 'react';
 
 interface SearchFormProps {
   term: string;
   onSearchFormSubmit: (search: string) => void;
 }
 
-interface SearchFormState {
-  search: string;
-  hasError: boolean;
-  loading: boolean;
-}
+const SearchForm: React.FC<SearchFormProps> = ({term, onSearchFormSubmit}) => {
+  const [search, setSearch] = useState(term);
+  const [hasError, setHasError] = useState(false);
 
-export default class SearchForm extends Component<
-  SearchFormProps,
-  SearchFormState
-> {
-  state = {
-    search: this.props.term,
-    hasError: false,
-    loading: false
-  };
+  const handleFormSubmit = (e: React.FormEvent) => {
+    onSearchFormSubmit(search.trim());
+    e.preventDefault();
+  }
 
-  componentDidUpdate(): void {
-    if (this.state.hasError) {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
+
+  const handleErrorBtnClick = () => {
+    setHasError(true);
+  }
+
+  useEffect( () => {
+    if (hasError) {
       throw new Error('Unexpected error');
     }
-  }
+  })
 
-  handleFormSubmit = (e: FormEvent): void => {
-    this.props.onSearchFormSubmit(this.state.search.trim());
-    e.preventDefault();
-  };
-
-  handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ search: e.target.value });
-  };
-
-  handleErrorBtnClick = (): void => {
-    this.setState({ hasError: true });
-  };
-
-  render(): ReactElement {
-    return (
-      <form className="searchForm" onSubmit={this.handleFormSubmit}>
-        <input
-          type="search"
-          value={this.state.search}
-          onChange={this.handleInputChange}
-        />
-        <button>Search</button>
-        <button type="button" onClick={this.handleErrorBtnClick}>
-          Error
-        </button>
-      </form>
-    );
-  }
+  return(
+    <form className="searchForm" onSubmit={handleFormSubmit}>
+      <input
+        type="search"
+        value={search}
+        onChange={handleInputChange}
+      />
+      <button>Search</button>
+      <button type="button" onClick={handleErrorBtnClick}>
+        Error
+      </button>
+    </form>
+  )
 }
+export default SearchForm;
